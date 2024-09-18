bcd81054393c1016fef995e8580447759460299d
Paul Whitehand <p.whitehand@uk.cdw.com>
2024-09-18 15:17:29 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello everybody, we are aren't we!
         </p>
         <a
           className="App-link"
