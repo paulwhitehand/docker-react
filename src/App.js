97ebe17b6b9862cb758a69b44acdae8f30d0707b
Paul Whitehand <p.whitehand@uk.cdw.com>
2024-09-19 12:47:11 +0100

    added txt to app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello everybody, we are aren't we!
+          Hello everybody, how are you today?
         </p>
         <a
           className="App-link"
